fa2dd382d43a5c5033d9c7863d7eeba903a4cb19
cunzaizhuyi <877824709@qq.com>
2022-05-29 15:11:10 +0800

    feat: 提交

---
+++ readme.md
@@ -0,0 +1,19 @@
+# m-ui
+
+
+# features
+
+* components
+* playground(given to developers)
+* utils
+* vue hooks(reactivity data and fns)
+
+# how to contribute
+* write your code
+* test your code in playground
+
+
+# on the shoulders of giants
+
+vue3 + vite2 + pnpm + (unocss + less)
+maybe vitest
---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import Unocss from 'unocss/vite';
+const path = require('path');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // Inspect(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      resolvers: [],
+      // Generate corresponding .eslintrc-auto-import.json file.
+      // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
+      eslintrc: {
+        enabled: false, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+    }),
+    Components({
+      dirs: ['src/components'],
+      resolvers: [],
+    }),
+    Unocss({
+      shortcuts: {
+      },
+    }),
+  ],
+  build: {
+  },
+  base: './',
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, '/src') },
+    ],
+  },
+  server: {
+    port: 3002,
+  },
+});
